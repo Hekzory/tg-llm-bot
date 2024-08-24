90adc49457403f1f34bd73bbdfb6ebf37e0f3bd1
Oleg <oleg-tsv@yandex.ru>
2024-08-24 20:54:18 +0300

    [*] model-service: increase handler timeout for big models, implement conversation-aware sql table structure, switch default test model to gemma2

--- go/model-service/internal/config/config.go
+++ go/model-service/internal/config/config.go
@@ -3,25 +3,29 @@ package config
 import "time"
 
 type Config struct {
-	ServerPort        int
-	DatabaseURL       string
-	ModelApiUrl       string
-	LogLevel          string
-	DBPollingInterval time.Duration
-	ModelName         string
+	ServerPort         int
+	DatabaseURL        string
+	ModelApiUrl        string
+	LogLevel           string
+	DBPollingInterval  time.Duration
+	ModelAnswerTimeout time.Duration
+	ModelName          string
 }
 
 func LoadConfig() (*Config, error) {
 
-	duration, _ := time.ParseDuration("5s")
+	pollingInterval, _ := time.ParseDuration("5s")
+	modelAnswerTimeout, _ := time.ParseDuration("10m")
 
 	cfg := &Config{
-		ServerPort:        1111,
-		DatabaseURL:       "postgresql://myuser:secret@db:5432/mydatabase",
-		ModelApiUrl:       "http://ollama:11434/",
-		LogLevel:          "DEBUG",
-		DBPollingInterval: duration,
-		ModelName:         "llama3.1:8b-instruct-q8_0",
+		ServerPort:         1111,
+		DatabaseURL:        "postgresql://myuser:secret@db:5432/mydatabase",
+		ModelApiUrl:        "http://ollama:11434/",
+		LogLevel:           "DEBUG",
+		DBPollingInterval:  pollingInterval,
+		ModelAnswerTimeout: modelAnswerTimeout,
+		// other considered choices are "mistral-nemo:12b-instruct-2407-q6_K", "llama3.1:70b-instruct-q4_K_S"
+		ModelName: "gemma2:27b-instruct-q6_K",
 	}
 	return cfg, nil
 }
--- sql/init.sql
+++ sql/init.sql
@@ -29,19 +29,38 @@ BEFORE UPDATE ON users
 FOR EACH ROW
 EXECUTE FUNCTION update_updated_at_column();
 
+-- Create conversations table
+CREATE TABLE IF NOT EXISTS conversations (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create an index on user_id for faster lookups
+CREATE INDEX IF NOT EXISTS idx_conversations_user_id ON conversations(user_id);
+
+-- Create a trigger to automatically update the updated_at column for conversations
+CREATE OR REPLACE TRIGGER update_conversations_updated_at
+BEFORE UPDATE ON conversations
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
+
 -- Create message_queue table
 CREATE TABLE IF NOT EXISTS message_queue (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id),
+    conversation_id INTEGER REFERENCES conversations(id) ON DELETE CASCADE,
     question TEXT NOT NULL,
     answer TEXT NOT NULL DEFAULT '',
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    status VARCHAR(20) CHECK (status IN ('new', 'processing', 'ready', 'answered')) DEFAULT 'new'
+    status VARCHAR(20) CHECK (status IN ('new', 'processing', 'ready', 'answered')) DEFAULT 'new',
+    tg_question_id BIGINT UNIQUE,
+    tg_answer_id BIGINT UNIQUE
 );
 
--- Create an index on user_id for faster lookups
-CREATE INDEX IF NOT EXISTS idx_message_queue_user_id ON message_queue(user_id);
+-- Create an index on conversation_id for faster lookups
+CREATE INDEX IF NOT EXISTS idx_message_queue_conversation_id ON message_queue(conversation_id);
 
 -- Create a trigger to automatically update the updated_at column for message_queue
 CREATE OR REPLACE TRIGGER update_message_queue_updated_at
